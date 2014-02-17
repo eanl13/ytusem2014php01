cdc4b3e892dae7d8974f23e03b51d24c62c3f4ca
eanl13 <eanl13@gmail.com>
2014-02-17 14:56:03 +0200

    Fonksiyonlar tanımlandı ödev bitirildi.

--- Odev2/functions.php
+++ Odev2/functions.php
@@ -13,10 +13,11 @@ $rastgeleAdet=rand(1,100);
 
 
 function stok_kontrol($_parametreRastgeleAd, $parametreRastgeleAdet){
+
+    global $meyveler;
+    global $rastgeleAd;
     if(is_int($_parametreRastgeleAd) && is_int($parametreRastgeleAdet)){
-        echo "ım the king";
-        meyve_var($_parametreRastgeleAd, $meyveler);
-        
+        meyve_var($meyveler,$rastgeleAd);
     }else{
         echo "hata oluştu";
         return false;
@@ -24,22 +25,15 @@ function stok_kontrol($_parametreRastgeleAd, $parametreRastgeleAdet){
 };
 
 
-function __construct($meyve_ad, $fiyat, $adet){
-    $this->meyve_ad = $meyve_ad;
-    $this->fiyat    = $fiyat;
-    $this->adet     = $adet;
-};
-
-
 $meyveler=array(
-    1  =>array('meyve_ad'=>'armut',      'fiyat'=>'0,75', 'adet'=>'23'),
-    2  =>array('meyve_ad'=>'muz',        'fiyat'=>'2,25', 'adet'=>'13'),
+    1  =>array('meyve_ad'=>'armut',      'fiyat'=>'5',    'adet'=>'53'),
+    2  =>array('meyve_ad'=>'muz',        'fiyat'=>'2,25', 'adet'=>'40'),
     3  =>array('meyve_ad'=>'mandalina',  'fiyat'=>'1,50', 'adet'=>'18'),
-    4  =>array('meyve_ad'=>'greyfurt',   'fiyat'=>'2,00', 'adet'=>'30'),
-    5  =>array('meyve_ad'=>'yenidunya',  'fiyat'=>'3,50', 'adet'=>'12'),
-    6  =>array('meyve_ad'=>'kavun',      'fiyat'=>'4,50', 'adet'=>'20'),
-    7  =>array('meyve_ad'=>'karpuz',     'fiyat'=>'3,50', 'adet'=>'17'),
-    8  =>array('meyve_ad'=>'erik',       'fiyat'=>'2,50', 'adet'=>'22'),
+    4  =>array('meyve_ad'=>'greyfurt',   'fiyat'=>'2,00', 'adet'=>'92'),
+    5  =>array('meyve_ad'=>'yenidunya',  'fiyat'=>'3,50', 'adet'=>'25'),
+    6  =>array('meyve_ad'=>'kavun',      'fiyat'=>'4,50', 'adet'=>'65'),
+    7  =>array('meyve_ad'=>'karpuz',     'fiyat'=>'3,50', 'adet'=>'78'),
+    8  =>array('meyve_ad'=>'erik',       'fiyat'=>'2,50', 'adet'=>'86'),
     9  =>array('meyve_ad'=>'dut',        'fiyat'=>'1,00', 'adet'=>'40'),
     10 =>array('meyve_ad'=>'uzum',       'fiyat'=>'5,50', 'adet'=>'34')
 );
@@ -47,11 +41,24 @@ $meyveler=array(
 
 stok_kontrol($rastgeleAd,$rastgeleAdet);
 
-function meyve_var($meyveler, $parametreRastgeleAd){
-    if(array_key_exists($parametreRastgeleAd, $meyveler)){
-        echo "key icinde var";
+function meyve_var($meyve, $parametreRastgeleAd){
+    global $rastgeleAdet, $meyveler;
+
+    if(array_key_exists($parametreRastgeleAd, $meyve)){
+
+        if($rastgeleAdet>=$meyveler['adet']){
+
+            $sonFiyat=$meyveler[$parametreRastgeleAd]['adet']*$meyveler[$parametreRastgeleAd]['fiyat'];
+            echo "meyve adı: {$meyveler[$parametreRastgeleAd]['meyve_ad']} <br>meyve adedi:{$meyveler[$parametreRastgeleAd]['adet']} <br>fiyatı:$sonFiyat";
+
+        }else{
+
+            echo "stokta yeteri kadar meyve yok";
+        }
     }else{
-        echo "key yok";
+        echo "meyve stokta bulunmuyor.";
     }
     
 };
+
+
